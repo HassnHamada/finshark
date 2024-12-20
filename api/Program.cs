ea6cc4edaedf82ac9ed452a9854cf2d78b3a3f04
HassnHamada <hassneltokhy4@gmail.com>
2024-12-20 15:07:47 +0200

    Clean test api point

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,5 @@
 using api.Data;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,12 +13,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// builder.Services.AddDbContext<ApplicationDBContext>(options => {
-//     var connetionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//     options.UseMySQL(connetionString, ServerVersion.AutoDetect(connetionString).ToString());
-// });
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,29 +24,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
