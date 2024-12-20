dc8e9e486a555da11f47af992b29db665bc39df8
HassnHamada <hassneltokhy4@gmail.com>
2024-12-20 19:18:48 +0200

    Add Comment GETALL API

---
+++ api/Mappers/CommentMapper.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Comment;
+using api.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace api.Mappers
+{
+    public static class CommentMapper
+    {
+        public static CommentDto ToDto(this Comment comment)
+        {
+            return new CommentDto
+            {
+                Id = comment.Id,
+                Title = comment.Title,
+                Content = comment.Content,
+                CreatedOn = comment.CreatedOn,
+                StockId = comment.StockId,
+            };
+        }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 var app = builder.Build();
 
