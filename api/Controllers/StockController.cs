af4731e95bddc8145251b5bb71739208396c41a4
HassnHamada <hassneltokhy4@gmail.com>
2024-12-20 17:14:44 +0200

    Add Stock PUT API

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -39,11 +39,29 @@ namespace api.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] CreateStockRequestDto stockDto){
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
             var stockModel = stockDto.ToStockFromCreateDto();
             _context.Stocks.Add(stockModel);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.Find(id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDto());
         }
     }
 }
